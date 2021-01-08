1263e8fe5150412d97dff3c23f79cb3981dccaa6
Joan_George <joangeorge1111@gmail.com>
2021-01-08 18:46:46 +0530

    updated http request and response

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, { useState} from 'react';
+import React, { useState ,useEffect} from 'react';
 import { EditorState,convertToRaw,convertFromHTML,ContentState,stateFromHTML} from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import htmlToDraft from 'html-to-draftjs';
 import draftToHtml from 'draftjs-to-html';
+import axios from 'axios';
 
 
 const App=() =>{
+
+  const USER_API_BASE_URL = 'http://192.168.1.15:8383/rawcode/';
+
+
 
   const blocksFromHTML = htmlToDraft(
     '<p>Hey <span style="color: rgb(235,107,86);">thi</span>s<strong>editor</strong>r<span style="color: rgb(26,188,156);">ocks</span>&nbsp;</p>'
@@ -19,8 +24,13 @@ const content = ContentState.createFromBlockArray(
     blocksFromHTML.contentBlocks,
     blocksFromHTML.entityMap
 );
+
+console.log(content)
   
   const [editorState, setEditorState] = useState(EditorState.createWithContent(content));
+  let data = {code:draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+
+useEffect(() =>{  console.log(axios.get(USER_API_BASE_URL+"get"))})
     
   return (
     <div className="App">
@@ -49,6 +59,7 @@ const content = ContentState.createFromBlockArray(
       disabled
       value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}></textarea>
       </div>
+      <button onClick={()=>{  axios.post(USER_API_BASE_URL+"add",data)}}>ClickMe</button>
     </div>
   );
 }
